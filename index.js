903851c2b8ad9d192a7843f27252f4a25b2876a3
Shad <ayazdyshin@gmail.com>
2022-03-28 20:04:59 +0200

    add functions for:
    computer choice, user choice, getting random number

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-function getRndInteger(min, max) {
+const arrOfOptions = ["rock", "paper" , "scissors"];
+
+const getRndInteger = (min, max) => {
     return Math.floor(Math.random() * (max - min) ) + min;
   }
-let computerPlay = () => {
-    let computerChoice = getRndInteger(1,4);
-    switch (computerChoice) {
-        case 1:
-            return "rock";
-        case 2:
-            return "paper";
-        case 3:
-            return "scissors";
-    }
+
+const computerPlay = () => {
+    let computerChoice = getRndInteger(0,3);
+    return arrOfOptions[computerChoice];
+}
+
+const getUserInput = () => {
+   let userInput = '';
+   while (!arrOfOptions.includes(userInput)){
+       userInput = prompt("Enter your choice:").toLowerCase();
+   }
+}
+
+const playRound = (userInput,computerChoice) => {
+
 }
