9fd953aceb9496060f6a97bb6f3df83336b33c32
Shad <ayazdyshin@gmail.com>
2022-03-30 19:40:29 +0200

    add function to play the whole game

--- script.js
+++ script.js
@@ -1,34 +1,49 @@
-const arrOfOptions = ["rock", "paper" , "scissors"];
+const arrOfOptions = ["rock", "paper", "scissors"];
 
 const getRndInteger = (min, max) => {
-    return Math.floor(Math.random() * (max - min) ) + min;
-  }
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 const computerPlay = () => {
-    let computerChoice = getRndInteger(0,3);
-    return arrOfOptions[computerChoice];
+  let computerChoice = getRndInteger(0, 3);
+  return arrOfOptions[computerChoice];
 }
 
 const getUserInput = () => {
-   let userInput = '';
-   while (!arrOfOptions.includes(userInput)){
-       userInput = prompt("Enter your choice:").toLowerCase();
-   }
-   return userInput;
+  let userInput = '';
+  while (!arrOfOptions.includes(userInput)) {
+    userInput = prompt("Enter your choice:").toLowerCase();
+  }
+  return userInput;
 }
 
-const playRound = (userInput,computerChoice) => {
-  switch(userInput) {
-      case (computerChoice) :
-          return "tie";
-      case ("rock") :
-           console.log("here");
-          return computerChoice === "paper" ? "you loose, paper beats rock" : "you win, rock beats scissors";
-      case ("paper") :
-         return computerChoice === "scissors" ? "you loose, scissors beat paper" : "you win, paper beats rock";
-      case ("scissors") :
-        return computerChoice === "rock" ? "you loose, rock beats scissor" : "you win, scissors beat paper";   
-      default:
-        return "some error";
+const playRound = (userInput, computerChoice) => {
+  switch (userInput) {
+    case (computerChoice):
+      return "tie";
+    case ("rock"):
+      return computerChoice === "paper" ? "you loose, paper beats rock" : "you win, rock beats scissors";
+    case ("paper"):
+      return computerChoice === "scissors" ? "you loose, scissors beat paper" : "you win, paper beats rock";
+    case ("scissors"):
+      return computerChoice === "rock" ? "you loose, rock beats scissor" : "you win, scissors beat paper";
+    default:
+      return "some error";
   }
 }
+
+const game = () => {
+  let playerScore = 0;
+  let computerScore = 0;
+  for (let i = 1; i <= 5; i++) {
+    let result = playRound(getUserInput(), computerPlay());
+    result.includes("you win") ? playerScore++ : computerScore++;
+    console.log(result);
+    if (playerScore === 3 || computerScore === 3) break;
+  }
+  if (playerScore === computerScore) console.log(`The score is: You:${playerScore} Computer:${computerScore} therefore it is a tie!`);
+  else if (playerScore > computerScore) console.log(`You won! The score is: You:${playerScore} Computer:${computerScore}`);
+  else if (playerScore < computerScore) console.log(`You loose! the score is: You:${playerScore} Computer:${computerScore}`);
+}
+
+game();
