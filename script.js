9da79e72eee2fc7e6bdba8ab3605fbb9eced3e03
Ayaz Dyshin <ayazdyshin@gmail.com>
2022-09-28 21:44:41 +0200

    split game logic in separate functions

--- script.js
+++ script.js
@@ -33,27 +33,31 @@ const playRound = (userInput, computerChoice) => {
       return "some error";
   }
 }
-
+let playerScore = 0;
+let computerScore = 0;
+const updateScore = (res) => {
+  if (res != 'tie') {
+    res.includes("you win") ? playerScore++ : computerScore++;
+    score.textContent = `Player: ${playerScore} Computer: ${computerScore}`;
+  };
+}
+const checkEnd = () => {
+  if (playerScore === 3 || computerScore === 3) {
+    if (playerScore > computerScore) alert(`You won! The score is: You:${playerScore} Computer:${computerScore}`);
+    else alert(`You loose! the score is: You:${playerScore} Computer:${computerScore}`);
+    playerScore = 0;
+    computerScore = 0;
+    currRes.textContent = '';
+    score.textContent = '';
+  }
+}
 const game = () => {
-  let playerScore = 0;
-  let computerScore = 0;
-
   const buttons = document.querySelectorAll('button');
   buttons.forEach((button) => {
     button.addEventListener('click', () => {
       currRes.textContent = playRound(button.textContent.toLowerCase(), computerPlay());
-      if (currRes.textContent != 'tie') {
-        currRes.textContent.includes("you win") ? playerScore++ : computerScore++;
-        score.textContent = `Player: ${playerScore} Computer: ${computerScore}`;
-      };
-      if (playerScore === 3 || computerScore === 3) {
-        if (playerScore > computerScore) alert(`You won! The score is: You:${playerScore} Computer:${computerScore}`);
-        else alert(`You loose! the score is: You:${playerScore} Computer:${computerScore}`);
-        playerScore = 0;
-        computerScore = 0;
-        currRes.textContent = '';
-        score.textContent = '';
-      }
+      updateScore(currRes.textContent);
+      checkEnd();
     });
   });
 }
